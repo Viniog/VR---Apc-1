51b145cd6ac197199fbf75b74660f359e09900d6
Viniog <160795953+Viniog@users.noreply.github.com>
2024-03-11 17:57:47 +0000

    save

---
+++ .ccls-cache/@home@runner@VR-Apc-1/aula@aula01@modificasdore.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+int numero;
+float flutuante;
+double juros_simples;
+
+int a;
+int x1, x2; // as da equação do 1o grau
+
+
+return 0;
+} 
---
+++ .ccls-cache/@home@runner@VR-Apc-1/aulas@aulas01@aula02@modificasdore.c
@@ -0,0 +1,15 @@
+#include <studio.h>
+
+int main() {
+  signed char caracter_com_sinal; // -128 a 127
+  unsigned char caracter_sem_sinal; // 0 a 255
+
+  signed int inteiro_com_sinal; // -2bi a 2bi
+  unsigned int inteiro_sem_sinal; // 0 a 4bi
+
+  short int inteiro_curto_com_sinal; // -32mil 2bytes 
+  unsigned short int inteiro_curto_sem_sinal; // 0 a 65mil 
+
+  long int inteiro_longo_com_sinal; // -9hexabi 8bytes 
+  unsigned long int inteiro_longo_sem_sinal; //0 a 
+}
