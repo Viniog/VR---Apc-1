592e7dd5b03630ce3da7a33e9c7744d9b5094067
Viniog <160795953+Viniog@users.noreply.github.com>
2024-03-14 00:52:23 +0000

    aulas - aulas 02

---
+++ aulas/aula01/aulas02/media.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() 
+{
+float nota1 = 0.0;
+float nota2 = 0.0;
+
+  printf("Entre com a nota1:\n ");
+  scanf("%f", &nota1);
+
+ printf("Entre com a nota2:\n ");
+  scanf("%f", &nota2);
+
+float media = 0.4* nota1 + 0.6*nota2;
+
+  printf("A media final é: %.1f\n", media);
+
+if (media >= 5.0) {
+  printf("Passei!\n");
+ else {
+  printf("Vamos de P3\n"); }
+return 0;
+}
